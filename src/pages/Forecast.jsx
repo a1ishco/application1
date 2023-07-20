224ce2911322a428ffad86bb405e5751b9e8ed7b
Ali Bashirov <ali.bashirov02@gmail.com>
2023-07-20 14:49:47 +0400

    Responsive design

--- src/pages/Forecast.jsx
+++ src/pages/Forecast.jsx
@@ -48,57 +48,66 @@ export default function Forecast() {
                         </div>))}
                 
                 </div>))} */}
-<div className="headerText mt-5">
-    <h1>Weather forecast for 5 days by hours</h1>
-    </div>
-             
-                <div className='row d-flex justify-content-center'>
+            <div className="headerText mt-5">
+                <h1>Weather forecast for 5 days by hours</h1>
+            </div>
 
-                    <input className='text-center mt-5 col-4' onKeyPress={handleKeyPress} onChange={onChangeInput} on style={{ width: '40%' }} type="text" id="message" placeholder='Enter the city: ex. Baku' />
-                    <button type="button" onClick={handleClick} class="btn btn-primary mt-5 g-0 col-1"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8" /><line x1="21" y1="21" x2="16.65" y2="16.65" /></svg></button>
-                </div>
-                <div className={show ? 'd-flex text-center justify-content-center' : 'd-none'}>
-                    <div className={mode}>
-                        <div className="card-body text-center">
-                            <h1>{info?.location?.name}</h1>
-                            <h6>{info?.location?.region}</h6>
-                            <h6>{info?.location?.country}</h6>
-                            <div className="row justify-content-md-center">
-                                <div className="dateCard">
-                                    <h4>{info?.forecast.forecastday.map((datas, i) => (
-                                        <div key={i}>
-                                            <div className='mb-3'>{datas?.date}</div>
-                                            <table class="table table-hover">
-                                                <tbody>
-                                                    <tr>
-                                                        <th>Hour</th>
-                                                        <th>Temperature C/F</th>
-                                                        <th>Weather</th>
-                                                        <th></th>
-                                                        <th>Wind</th>
-                                                        <th>Rain</th>
-                                                    </tr>
+            <div className='row d-flex justify-content-center'>
 
-                                                    {datas.hour.map((hours, j) => (
-                                                        <tr key={j}>
-                                                            <td>{(hours?.time).slice(10,16)}</td>
-                                                            <td>{hours?.temp_c}C / {hours?.temp_f}F</td>
-                                                            <td>{hours?.condition.text}</td>
-                                                            <td><img width="50px" src={hours?.condition.icon} /></td>
-                                                            <td>{hours?.wind_kph} km/h ({hours?.wind_dir})</td>
-                                                            <td>{hours?.chance_of_rain}%</td>
+                <input className='text-center mt-5 col-4' onKeyPress={handleKeyPress} onChange={onChangeInput} on style={{ width: '40%' }} type="text" id="message" placeholder='Enter the city: ex. Baku' />
+                <button type="button" onClick={handleClick} class="btn
+                 btn-primary mt-5 g-0 col-1"><svg className='svgIcon' xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8" /><line x1="21" y1="21" x2="16.65" y2="16.65" /></svg></button>
+            </div>
+            <div className="row d-flex m-0 justify-content-center col-12 col-">
+                <div className="container">
+                    <div className={show ? 'col-12 col-md-12 col-lg-12 d-flex text-center justify-content-center' : 'd-none'}>
+                        <div className={mode}>
+                            <div className="card-body text-center">
+                                <div className="justify-content-md-center">
+                                    <div className="card-title">
+                                        <h1>{info?.location?.name}</h1>
+                                        <h6>{info?.location?.region}</h6>
+                                        <h6>{info?.location?.country}</h6>
+                                    </div>
+                                    <div className="dateCard">
+                                        
+                                        <h4>{info?.forecast.forecastday.map((datas, i) => (
+                                            <div key={i}>
+
+                                                <div className='mb-3'>{datas?.date}</div>
+                                                <table class="table table-hover">
+                                                    <tbody>
+                                                        <tr>
+                                                            <th>Hour</th>
+                                                            <th>Temperature C/F</th>
+                                                            <th>Weather</th>
+                                                            <th>Wind</th>
+                                                            <th>Rain</th>
                                                         </tr>
-                                                    ))}
-                                            </tbody>
-                                        </table>
-                                        </div>))}</h4>
+
+                                                        {datas.hour.map((hours, j) => (
+                                                            <tr key={j}>
+                                                                <td>{(hours?.time).slice(10, 16)}</td>
+                                                                <td>{hours?.temp_c}C <hr/> {hours?.temp_f}F</td>
+                                                                <td>{hours?.condition.text}<hr/><img width="50px" src={hours?.condition.icon} /></td>
+                                                                <td>{hours?.wind_kph} km/h <hr/>({hours?.wind_dir})</td>
+                                                                <td>{hours?.chance_of_rain}%</td>
+                                                            </tr>
+                                                        ))}
+                                                    </tbody>
+                                                </table>
+                                            </div>))}</h4>
 
 
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
+
             </div>
-        </div>
+
         </main >
     );
 }
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -36,7 +36,7 @@ export default function MainPage() {
   var mode = "'card mt-5 col-6 bg-dark text-light";
   const hour = (((info?.location?.localtime)?.slice(11, 13)));
   if ((hour >= 6 && hour <= 20)) {
-    mode = "card mt-5 col-6 bg-light text-dark"
+    mode = "card mt-5 col-lg-12 col-md-6 bg-light text-dark"
   }
 
   //Rendering
@@ -51,7 +51,8 @@ export default function MainPage() {
           <input className='text-center mt-5 col-4' onKeyPress={handleKeyPress} onChange={onChangeInput} on style={{ width: '40%' }} type="text" id="message" placeholder='Enter the city: ex. Baku' />
           <button type="button" onClick={handleClick} class="btn btn-primary mt-5 g-0 col-1"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8" /><line x1="21" y1="21" x2="16.65" y2="16.65" /></svg></button>
         </div>
-        <div className={show ? 'd-flex text-center justify-content-center' : 'd-none'}>
+        <div className="row justify-content-md-center mb-5">
+          <div className={show ? 'col-12 col-md-12 col-lg-6 d-flex text-center justify-content-center' : 'd-none'}>
         <div className={mode}>
           <div className="card-body text-center">
             <h1>{info?.location?.name}</h1>
@@ -73,6 +74,8 @@ export default function MainPage() {
           </div>
         </div>
         </div>
+        </div>
+          
       </div>
     
   );
