82912fc31981de5d5e2488eac4527cae04ce193a
Ali Bashirov <ali.bashirov02@gmail.com>
2023-07-24 12:40:29 +0400

    Bug fixed

--- src/pages/Forecast.jsx
+++ src/pages/Forecast.jsx
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import "./styles.css"
+import { Spinner } from 'react-bootstrap';
 
 export default function Forecast() {
     const [info, setInfo] = useState(null);
     const [inputValue, setInputValue] = useState(null);
     const [show, setShow] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(false);
 
     //Input change function
     function onChangeInput(e) {
@@ -19,20 +21,23 @@ export default function Forecast() {
 
     //Search button function
     function handleClick() {
-        setShow(true)
+        setErrorMessage(false)
         var FETCH_URL = 'https://api.weatherapi.com/v1/forecast.json?key=a3b5720f8c00466f91a142434231807&q=' + inputValue + '&days=5&aqi=no&alerts=no';
         axios.get(FETCH_URL).then((response) => {
             setInfo(response.data);
-            console.log(response.data);
+            setShow(true)
         }).catch((err) => {
             console.log(err);
+            setErrorMessage(true)
+            setShow(false)
         })
             .finally(() => {
             });
+            
 
     }
 
-    var mode = "'card mt-5 col-12 bg-light text-dark g-2";
+    var mode = "card mt-5 col-12 bg-light text-dark g-2";
 
     //Rendering
     return (
@@ -58,8 +63,13 @@ export default function Forecast() {
                 <button type="button" onClick={handleClick} class="btn
                  btn-primary mt-5 g-0 col-1"><svg className='svgIcon' xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8" /><line x1="21" y1="21" x2="16.65" y2="16.65" /></svg></button>
             </div>
-            <div className="row d-flex m-0 justify-content-center col-12 col-">
-                <div className="container">
+            <div className="row d-flex m-0 justify-content-center col-12">
+                <div className='container'>
+                    <div className={errorMessage ? "message mt-3" : "d-none"}>
+                        <h1 className='errorMessageTxtH1'>Error occured</h1>
+                        <Spinner />
+                        <h5 className='errorMessageTxtH5'>Please check or change location</h5>
+                    </div>
                     <div className={show ? 'col-12 col-md-12 col-lg-12 d-flex text-center justify-content-center' : 'd-none'}>
                         <div className={mode}>
                             <div className="card-body text-center">
@@ -70,7 +80,7 @@ export default function Forecast() {
                                         <h6>{info?.location?.country}</h6>
                                     </div>
                                     <div className="dateCard">
-                                        
+
                                         <h4>{info?.forecast.forecastday.map((datas, i) => (
                                             <div key={i}>
 
@@ -88,9 +98,9 @@ export default function Forecast() {
                                                         {datas.hour.map((hours, j) => (
                                                             <tr key={j}>
                                                                 <td>{(hours?.time).slice(10, 16)}</td>
-                                                                <td>{hours?.temp_c}C <hr/> {hours?.temp_f}F</td>
-                                                                <td>{hours?.condition.text}<hr/><img width="50px" src={hours?.condition.icon} /></td>
-                                                                <td>{hours?.wind_kph} km/h <hr/>({hours?.wind_dir})</td>
+                                                                <td>{hours?.temp_c}C <hr /> {hours?.temp_f}F</td>
+                                                                <td>{hours?.condition.text}<hr /><img width="50px" src={hours?.condition.icon} /></td>
+                                                                <td>{hours?.wind_kph} km/h <hr />({hours?.wind_dir})</td>
                                                                 <td>{hours?.chance_of_rain}%</td>
                                                             </tr>
                                                         ))}
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,30 +1,34 @@
 import axios from 'axios';
 import React, { useState } from 'react';
+import { Spinner } from 'react-bootstrap';
 
 
 export default function MainPage() {
   const [info, setInfo] = useState(null);
   const [inputValue, setInputValue] = useState(null);
   const [show, setShow] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
 
   //Input change function
   function onChangeInput(e) {
     setInputValue(e.target.value)
   }
-  function handleKeyPress(event){
-    if(event.key === 'Enter'){
+  function handleKeyPress(event) {
+    if (event.key === 'Enter') {
       handleClick()
     }
   }
 
   //Search button function
   function handleClick() {
-    setShow(true)
+    setErrorMessage(false)
     var FETCH_URL = 'https://api.weatherapi.com/v1/current.json?key=a3b5720f8c00466f91a142434231807&q=' + inputValue + '&aqi=no';
     axios.get(FETCH_URL).then((response) => {
       setInfo(response.data);
+      setShow(true)
     }).catch((err) => {
-      console.log(err);
+      setErrorMessage(true)
+      setShow(false)
     })
       .finally(() => {
       });
@@ -43,43 +47,48 @@ export default function MainPage() {
   return (
     <div>
 
-<div className="headerText mt-5">
-    <h1>Weather for today</h1>
-    </div>
-        <div className='row d-flex justify-content-center'>
+      <div className="headerText mt-5">
+        <h1>Weather for today</h1>
+      </div>
+      <div className='row d-flex justify-content-center'>
 
-          <input className='text-center mt-5 col-4' onKeyDown={handleKeyPress} onChange={onChangeInput} on style={{ width: '40%' }} type="text" id="message" placeholder='Enter the city: ex. Baku' />
-          <button type="button" onClick={handleClick} class="btn btn-primary mt-5 g-0 col-1"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8" /><line x1="21" y1="21" x2="16.65" y2="16.65" /></svg></button>
-        </div>
-        <div className="row justify-content-md-center mb-5">
-          <div className={show ? 'col-12 col-md-12 col-lg-6 d-flex text-center justify-content-center' : 'd-none'}>
-        <div className={mode}>
-          <div className="card-body text-center">
-            <h1>{info?.location?.name}</h1>
-            <h6>{info?.location?.region}</h6>
-            <h6>{info?.location?.country} - {(info?.location?.localtime)?.slice(0, 10)}</h6>
-            <hr />
-            <div className="container">
-              <div className="row justify-content-center">
-              <h4>{info?.current?.condition?.text}<img width="50px" src={info?.current?.condition.icon} />
-              </h4>
-              <div className="row d-inline justify-content-center g-0">
-                <div className='alert alert-primary col-12 col-md-12 mb-3 text-break'>
-                  <h4>{info?.current?.temp_c} C / {info?.current?.temp_f} F</h4>
-                </div>
-                <div className='alert alert-primary col-12 col-md-12 mb-3 text-break'>
-                  <h4>Wind: {info?.current?.wind_kph} km/h ({info?.current?.wind_dir})</h4>
+        <input className='text-center mt-5 col-4' onKeyDown={handleKeyPress} onChange={onChangeInput} on style={{ width: '40%' }} type="text" id="message" placeholder='Enter the city: ex. Baku' />
+        <button type="button" onClick={handleClick} class="btn btn-primary mt-5 g-0 col-1"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8" /><line x1="21" y1="21" x2="16.65" y2="16.65" /></svg></button>
+      </div>
+      <div className="row justify-content-md-center mb-5">
+      <div className={errorMessage ? "message mt-3" : "d-none"}>
+                        <h1 className='errorMessageTxtH1'>Error occured</h1>
+                        <Spinner />
+                        <h5 className='errorMessageTxtH5'>Please check or change location</h5>
+                    </div>
+        <div className={show ? 'col-12 col-md-12 col-lg-6 d-flex text-center justify-content-center' : 'd-none'}>
+          <div className={mode}>
+            <div className="card-body text-center">
+              <h1>{info?.location?.name}</h1>
+              <h6>{info?.location?.region}</h6>
+              <h6>{info?.location?.country} - {(info?.location?.localtime)?.slice(0, 10)}</h6>
+              <hr />
+              <div className="container">
+                <div className="row justify-content-center">
+                  <h4>{info?.current?.condition?.text}<img width="50px" src={info?.current?.condition.icon} />
+                  </h4>
+                  <div className="row d-inline justify-content-center g-0">
+                    <div className='alert alert-primary col-12 col-md-12 mb-3 text-break'>
+                      <h4>{info?.current?.temp_c} C / {info?.current?.temp_f} F</h4>
+                    </div>
+                    <div className='alert alert-primary col-12 col-md-12 mb-3 text-break'>
+                      <h4>Wind: {info?.current?.wind_kph} km/h ({info?.current?.wind_dir})</h4>
+                    </div>
+                  </div>
                 </div>
               </div>
+
             </div>
-            </div>
-            
           </div>
         </div>
-        </div>
-        </div>
-          
       </div>
-    
+
+    </div>
+
   );
 }
